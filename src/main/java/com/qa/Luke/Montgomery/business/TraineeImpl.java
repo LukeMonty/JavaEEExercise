28b47f88ce07cdad1475a23a5f889350821adc2c
Luke Montgomery <luke.p.montgomery@gmail.com>
2019-02-01 12:05:55 +0000

    Added business layer for Trainee

--- src/main/java/com/qa/Luke/Montgomery/business/TraineeImpl.java
+++ src/main/java/com/qa/Luke/Montgomery/business/TraineeImpl.java
@@ -1,5 +1,37 @@
 package com.qa.Luke.Montgomery.business;
 
-public class TraineeImpl {
+import javax.inject.Inject;
+
+import com.qa.Luke.Montgomery.persistence.repository.TraineeInterface;
+
+public class TraineeImpl implements TraineeBusInterface {
+	
+	@Inject
+	private TraineeInterface repo;
+
+	@Override
+	public String getAllTrainees() {
+		return repo.getAllTrainees();
+	}
+
+	@Override
+	public String getAllTraineesInClassroom(Long id) {
+		return repo.getAllTraineesInClassroom(id);
+	}
+
+	@Override
+	public String amendTrainee(Long id, String updatedTrainee) {
+		return repo.amendTrainee(id, updatedTrainee);
+	}
+
+	@Override
+	public String createTrainee(String trainee) {
+		return repo.createTrainee(trainee);
+	}
+
+	@Override
+	public String removeTrainee() {
+		return repo.removeTrainee(id);
+	}
 
 }
